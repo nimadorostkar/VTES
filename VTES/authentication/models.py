b975d8b7b1e7ee5c4769fc540ee8cce5b39c1a48
nimadorostkar <nimadorostkar97@gmail.com>
2022-02-17 01:48:48 +0330

    migrations....

--- VTES/authentication/models.py
+++ VTES/authentication/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+from django.urls import reverse
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.template.defaultfilters import truncatechars
+from django_jalali.db import models as jmodels
 
-# Create your models here.
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE,unique=True,related_name='profile',verbose_name = "کاربر")
+  phone = models.CharField(max_length=50,null=True, blank=True,verbose_name = " شماره تماس  ")
+  date_created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "تاریخ ایجاد")
+
+  def __str__(self):
+    return str(self.user)
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+      if created:
+          Profile.objects.create(user=instance)
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+      instance.profile.save()
+
+  def get_absolute_url(self):
+      return reverse('user_detail',args=[self.id])
+
+  class Meta:
+      verbose_name = "کاربر"
+      verbose_name_plural = "کاربران"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#End
--- VTES/authentication/urls.py
+++ VTES/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view, register_user, password_change
+from .views import login_view, register_user
 from django.contrib.auth.views import LogoutView
 
 
