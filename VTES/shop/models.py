d8711f82d68f594cbf6bacf41d8b623f17d57294
nimadorostkar <nimadorostkar97@gmail.com>
2022-02-17 14:00:17 +0330

    create Product model

--- VTES/shop/models.py
+++ VTES/shop/models.py
@@ -33,6 +33,8 @@ class Category(MPTTModel):
 
 
 
+
+
 #------------------------------------------------------------------------------
 class Shop(models.Model):
   user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile', verbose_name = "کاربر")
@@ -42,7 +44,7 @@ class Shop(models.Model):
   email = models.EmailField(max_length=50, null=True, blank=True, verbose_name = "ایمیل")
   address = models.CharField(max_length=200, null=True, blank=True, verbose_name = "آدرس")
   description = models.TextField(max_length=1000,null=True, blank=True, verbose_name = "توضیحات")
-  category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category', verbose_name = "دسته بند")
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='shop_category', verbose_name = "دسته بند")
   date_created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "تاریخ ایجاد")
 
   def __str__(self):
@@ -65,6 +67,39 @@ class Shop(models.Model):
 
 
 
+
+
+
+
+#------------------------------------------------------------------------------
+class Product(models.Model):
+    approved = models.BooleanField(default=False, verbose_name = "تایید شده")
+    available = models.BooleanField(default=True, verbose_name = "موجود")
+    provider_shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='shop', verbose_name = "فروشگاه ارائه دهنده")
+    code = models.CharField(max_length=50, verbose_name = "کد محصول")
+    name = models.CharField(max_length=80, verbose_name = "نام محصول")
+    image = models.ImageField(default='products/default.png', upload_to='products', verbose_name = "تصویر")
+    price = models.IntegerField(verbose_name = "قیمت")
+    qty = models.IntegerField(verbose_name = "تعداد")
+    brand = models.CharField(max_length=50, verbose_name = "برند محصول")
+    link = models.URLField(max_length=200, verbose_name = "لینک محصول")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product_category', verbose_name = "دسته بند")
+    description = models.TextField(max_length=1000,null=True, blank=True, verbose_name = "توضیحات")
+    datasheet = models.FileField(upload_to='datasheet', max_length=254, verbose_name = "فایل و Datasheet")
+    date_created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "تاریخ ایجاد")
+
+    def __str__(self):
+        return str(self.name)
+
+    def img_tag(self):
+        return format_html("<img width=40 src='{}'>".format(self.image.url))
+
+    def get_absolute_url(self):
+        return reverse('product_detail',args=[self.id])
+
+    class Meta:
+        verbose_name = "محصول"
+        verbose_name_plural = "محصولات"
 
 
 
