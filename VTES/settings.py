b7b31ee0f26435533093cefdcaa5184c8f3fa7e1
nima <54316106+nimadorostkar@users.noreply.github.com>
2022-03-09 18:41:12 +0330

    Update settings.py

--- VTES/settings.py
+++ VTES/settings.py
@@ -111,6 +111,13 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME'  : 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ddql0vvehcrtcn',
         'USER': 'awbasueredkqxx',
@@ -119,6 +126,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
 
 # Password validation
