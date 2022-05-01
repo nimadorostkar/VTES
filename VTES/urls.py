f8553a8320f4a56d3fe16114b33d4641b12ef097
Fan Wang <nimadorostkar97@gmail.com>
2022-05-01 17:05:01 +0430

    index page

--- VTES/urls.py
+++ VTES/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
     path('api/', include('shop.urls')),
     path('api/', include('authentication.urls')),
 ]
