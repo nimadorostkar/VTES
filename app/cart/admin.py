66282e9ae16cb1ad5cba52befa33f9148778f877
Fan Wang <nimadorostkar97@gmail.com>
2022-06-15 02:15:12 +0430

    cart create and updated

--- app/cart/admin.py
+++ app/cart/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('place','address', 'phone_number', 'user')
+    list_filter = ("user", "place")
+    search_fields = ['address', 'phone_number', 'postal_code']
+    raw_id_fields = ('user'),
+admin.site.register(models.Address, AddressAdmin)
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class ShippingTimeAdmin(admin.ModelAdmin):
+    list_display = ('date', )
+    list_filter = ("date", )
+admin.site.register(models.ShippingTime, ShippingTimeAdmin)
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', )
+    list_filter = ("user", )
+admin.site.register(models.Cart, CartAdmin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#End
--- app/cart/models.py
+++ app/cart/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.dispatch import receiver
 from authentication.models import User
+from shop.models import ShopProducts
+
 
 
 
@@ -12,7 +14,7 @@ from authentication.models import User
 
 #------------------------------------------------------------------------------
 class Address(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile', verbose_name = "کاربر")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_address', verbose_name = "کاربر")
     CHOICES = ( ('خانه','خانه'), ('فروشگاه','فروشگاه'))
     place = models.CharField(max_length=256, choices=CHOICES, verbose_name = "محل")
     address = models.CharField(max_length=256, verbose_name = "آدرس")
@@ -25,3 +27,65 @@ class Address(models.Model):
     class Meta:
         verbose_name = "آدرس"
         verbose_name_plural = "آدرس ها"
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class ShippingTime(models.Model):
+    date = models.DateField(verbose_name = "تاریخ")
+    CHOICES = ( ('۹ تا ۱۲','۹ تا ۱۲'), ('۱۲ تا ۱۵','۱۲ تا ۱۵'), ('۱۵ تا ۱۸','۱۵ تا ۱۸'), ('۱۸ تا ۲۱','۱۸ تا ۲۱') )
+    time = models.CharField(max_length=256, choices=CHOICES, verbose_name = "ساعت")
+
+
+    def __str__(self):
+        return str(self.date) + str(self.time)
+
+    class Meta:
+        verbose_name = "زمان ارسال"
+        verbose_name_plural = "زمان ارسال"
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_user', verbose_name = "کاربر")
+    products = models.ManyToManyField(ShopProducts, verbose_name = "محصولات")
+
+
+    def __str__(self):
+        return str(self.user) + str(self.products)
+
+    class Meta:
+        verbose_name = "سبد خرید"
+        verbose_name_plural = "سبد های خرید"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#End
