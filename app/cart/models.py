794bbe9d96a7b05f8b0c7a25128db07905069435
Fan Wang <nimadorostkar97@gmail.com>
2022-06-28 12:35:36 +0430

    cart models

--- app/cart/models.py
+++ app/cart/models.py
@@ -9,13 +9,31 @@ from shop.models import ShopProducts
 
 
 
+#------------------------------------------------------------------------------
+class PostWay(models.Model):
+    way = models.CharField(max_length=60, verbose_name='روش سفارش')
+    price = models.IntegerField(verbose_name='هزینه ارسال')
+    selected = models.BooleanField(default=False, verbose_name='روش پست منتخب')
+
+    class Meta:
+        verbose_name = 'روش‌‌ ارسال'
+        verbose_name_plural = 'روش‌های ارسال'
+
+    def __str__(self):
+        return self.way
+
+
+
+
+
+
 
 
 
 #------------------------------------------------------------------------------
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_address', verbose_name = "کاربر")
-    CHOICES = ( ('خانه','خانه'), ('فروشگاه','فروشگاه'))
+    CHOICES = ( ('خانه','خانه'), ('فروشگاه','فروشگاه'), ('انبار','انبار'))
     place = models.CharField(max_length=256, choices=CHOICES, verbose_name = "محل")
     address = models.CharField(max_length=256, verbose_name = "آدرس")
     phone_number = models.CharField(max_length=256, verbose_name = "شماره تماس")
@@ -61,18 +79,68 @@ class ShippingTime(models.Model):
 
 
 
+
 #------------------------------------------------------------------------------
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_user', verbose_name = "کاربر")
-    products = models.ManyToManyField(ShopProducts, verbose_name = "محصولات")
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    product = models.ForeignKey(ShopProducts, on_delete=models.CASCADE, verbose_name='محصول')
+    quantity = models.IntegerField(verbose_name='تعداد')
 
     def __str__(self):
-        return str(self.user) + str(self.products)
+        return self.product.product.name
 
     class Meta:
-        verbose_name = "سبد خرید"
-        verbose_name_plural = "سبد های خرید"
+        verbose_name = 'سبد خرید'
+        verbose_name_plural = 'سبدهای خرید'
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+class Order(models.Model):
+    STATUS = (
+        ('New', 'جدید'),
+        ('Accepted', 'تایید سفارش'),
+        ('Preparing', 'آماده سازی سفارش'),
+        ('InPostOffice', 'تحویل به پست'),
+        ('Arrive', 'تحویل به مشتری'),
+        ('Canceled', 'لغو شده'),
+    )
+    PAY_WAY = ( ('online', 'پرداخت آنلاین'), ('credit', 'اعتباری'))
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='کاربر')
+    code = models.CharField(max_length=10, editable=False, verbose_name='کد سفارش')
+    carts = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True, verbose_name='محصولات')
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, verbose_name='آدرس')
+    delivery_time = models.ForeignKey(ShippingTime, on_delete=models.SET_NULL, null=True, verbose_name='زمان تحویل')
+    post_way = models.ForeignKey(PostWay, on_delete=models.SET_NULL, null=True, verbose_name='نحوه ارسال')
+    pay_way = models.CharField(max_length=50, choices=PAY_WAY, default='online', verbose_name='نحوه پرداخت')
+    total = models.IntegerField(verbose_name='جمع مبلغ کل سفارشات')
+    amount = models.IntegerField(verbose_name='جمع تعداد کل سفارشات')
+    status = models.CharField(max_length=30, choices=STATUS, default='New', verbose_name='وضعیت')
+    admin_note = models.CharField(blank=True, max_length=100, verbose_name='یادداشت ادمین')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='ایجاد شده')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'سفارش'
+        verbose_name_plural = 'سفارشات'
+
+
+
+
+
 
 
 
