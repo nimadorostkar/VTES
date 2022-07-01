51529a184e27afea730d0fa01d23cf4bd0056963
Fan Wang <nimadorostkar97@gmail.com>
2022-07-02 03:37:30 +0430

    wsgi

--- VTES/wsgi.py
+++ VTES/wsgi.py
@@ -1,2 +1,9 @@
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+import os
+from django.core.wsgi import get_wsgi_application
+
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VTES.settings')
+
+application = get_wsgi_application()
