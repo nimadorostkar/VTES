9e452a5a99b0e4484abff131bd9ba422ef82fcd6
Fan Wang <nimadorostkar97@gmail.com>
2022-07-21 11:06:06 +0430

    Province and City in model

--- shop/models.py
+++ shop/models.py
@@ -14,6 +14,47 @@ from colorfield.fields import ColorField
 
 
 
+
+
+
+
+
+#------------------------------------------------------------------------------
+class Province(models.Model):
+    name = models.CharField(max_length=256, unique=True, verbose_name="نام")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "استان"
+        verbose_name_plural = "استان ها"
+
+
+#------------------------------------------------------------------------------
+class City(models.Model):
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, verbose_name = "استان")
+    name = models.CharField(max_length=256, unique=True, verbose_name="نام")
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = "استان"
+        verbose_name_plural = "استان ها"
+
+
+
+
+
+
+
+
+
+
+
+
+
 #------------------------------------------------------------------------------
 class Category(MPTTModel):
     name = models.CharField(max_length=50, unique=True, verbose_name = "نام")
