b404602d00803edc54db95c4e94403e16810dc36
Fan Wang <nimadorostkar97@gmail.com>
2022-07-22 21:49:39 +0430

    push

--- ticket/models.py
+++ ticket/models.py
@@ -16,9 +16,9 @@ from authentication.models import User
 #------------------------------------------------------------------------------
 class Ticket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name = "کاربر")
-    subject = models.CharField(max_length=256, verbose_name="موضوع")
-    type = models.CharField(max_length=256, verbose_name="نوع")
-    description = models.TextField()
+    subject = models.CharField(max_length=256, null=True, blank=True, verbose_name="موضوع")
+    type = models.CharField(max_length=256, null=True, blank=True, verbose_name="نوع")
+    description = models.TextField(verbose_name="توضیحات")
 
     def __str__(self):
         return str(self.subject) + "|" + str(self.user)
