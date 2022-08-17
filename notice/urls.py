389a2d36bab5438ba88237a4679adcee626756f8
Fan Wang <nimadorostkar97@gmail.com>
2022-08-17 14:54:54 +0430

    pushhh

--- notice/urls.py
+++ notice/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
   #path('partner_notice', views.PartnerNotice.as_view(), name='partner_notice'),
-  path('partner_req', views.PartnerReq.as_view(), name='partner_req'),
-  path('partner_req_item/<int:id>', views.PartnerReqItem.as_view(), name='partner_req_item'),
+  path('partner_req_notice', views.PartnerReq.as_view(), name='partner_req_notice'),
+  path('partner_req_item_notice/<int:id>', views.PartnerReqItem.as_view(), name='partner_req_item_notice'),
 
 ]
