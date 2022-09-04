7471a6f363dfb92433993f3673141f2ec1605f21
Fan Wang <nimadorostkar97@gmail.com>
2022-09-04 14:28:43 +0430

    remove exchange product model

--- partners/admin.py
+++ partners/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from . import models
-from .models import ExchangePartner, ExchangeReq
+from .models import ExchangePartner
 from import_export.admin import ImportExportModelAdmin, ImportExportMixin
 
 
