10b3a19b95e2afb27f5170e0487d943f9d8a5e98
Fan Wang <nimadorostkar97@gmail.com>
2022-09-14 15:38:45 +0430

    AddressItem

--- cart/urls.py
+++ cart/urls.py
@@ -7,7 +7,10 @@ from . import views
 urlpatterns = [
   path('add_to_cart', views.AddToCart.as_view(), name='add_to_cart'),
   path('add_to_cart/<int:id>', views.AddToCartItem.as_view(), name='add_to_cart'),
+  #
   path('address', views.Address.as_view(), name='address'),
+  path('address_item/<int:id>', views.AddressItem.as_view(), name='address_item'),
+  #
   path('order', views.Order.as_view(), name='order'),
   path('postway', views.PostWay.as_view(), name='postway'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -68,6 +68,38 @@ class Address(APIView):
 
 
 
+#----------------------------------------------------- AddressItem -------------
+class AddressItem(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        query = get_object_or_404(models.Address, id=self.kwargs["id"])
+        serializer = AddressSerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        request.data['user']=request.user.id
+        query = get_object_or_404(models.Address, id=self.kwargs["id"])
+        serializer = AddressSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, *args, **kwargs):
+        query = get_object_or_404(models.Address, id=self.kwargs["id"])
+        query.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
 
 
 
